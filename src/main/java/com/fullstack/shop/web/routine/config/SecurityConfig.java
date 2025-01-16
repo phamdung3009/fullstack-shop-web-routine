d5c6946e900e0baedebe8d2546b2ae9f75e91569
phamdung3009 <hoangdung1999.vn@gmail.com>
2025-01-16 15:51:35 +0700

    format code

--- src/main/java/com/fullstack/shop/web/routine/config/SecurityConfig.java
+++ src/main/java/com/fullstack/shop/web/routine/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.fullstack.shop.web.routine.config;
 
 import lombok.experimental.NonFinal;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
--- src/main/java/com/fullstack/shop/web/routine/service/AuthenticationService.java
+++ src/main/java/com/fullstack/shop/web/routine/service/AuthenticationService.java
@@ -14,7 +14,6 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
